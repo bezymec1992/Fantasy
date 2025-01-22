532d31d2a84c5ff8a86bad3a330c9ac75f2201a6
Vasiliy Lavrov <68432186+bezymec1992@users.noreply.github.com>
2025-01-22 19:06:04 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,84 @@
+const signupForms = document.querySelectorAll('.form');
+const passwordInput = document.getElementById('password');
+const togglePassword = document.getElementById('togglePassword');
+
+// Toggle password visibility
+togglePassword.addEventListener('click', () => {
+  const type = passwordInput.type === 'password' ? 'text' : 'password';
+  passwordInput.type = type;
+});
+
+// Form submission with validation
+signupForms.forEach((form) => {
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    let valid = true;
+    if (valid) {
+      window.location.href = 'product.html'; // Замените на нужный URL
+    }
+  });
+});
+
+//second form < 768px
+const maleButton = document.getElementById('maleButton');
+const femaleButton = document.getElementById('femaleButton');
+
+//chose gender btn
+function activateButton(button) {
+  maleButton.classList.remove('active');
+  femaleButton.classList.remove('active');
+  button.classList.add('active');
+}
+
+maleButton.addEventListener('click', () => activateButton(maleButton));
+femaleButton.addEventListener('click', () => activateButton(femaleButton));
+
+
+
+
+const swiper = new Swiper('.swiper', {
+  direction: 'horizontal',
+  loop: true,
+  
+  grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  spaceBetween: 30,
+  initialSlide: 1, 
+  centeredSlides: true,
+
+  slidesPerView: 1,
+  loopAdditionalSlides: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 2.5,
+      centeredSlides: true,
+    }
+  }
+});
+
+const swiper2 = new Swiper('.swiper2', {
+  direction: 'horizontal',
+  loop: true,
+  
+  grabCursor: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
+  spaceBetween: 30,
+  initialSlide: 1, 
+  centeredSlides: true,
+
+  slidesPerView: 1,
+  loopAdditionalSlides: 2,
+  breakpoints: {
+    768: {
+      slidesPerView: 3.8,
+      centeredSlides: true,
+    }
+  }
+});
+
